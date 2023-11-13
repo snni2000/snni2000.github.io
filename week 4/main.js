f0622480c228d8f575380ab24f4842f43132ac50
Shoaib Naeem <snni2000@hw.ac.uk>
2023-11-13 10:57:03 +0400

    4

--- week 4/main.js
+++ week 4/main.js
@@ -1,5 +1,5 @@
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
+ourRequest.open('GET', 'https://github.com/snni2000/snni2000.github.io/blob/main/week%204/cities1.json');
 ourRequest.onload = function() {
 console.log(ourRequest.responseText);
 };
